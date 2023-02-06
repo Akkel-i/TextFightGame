fe3f88f94c985bd082c9c23aa85a6e3f23a10064
Akkel-i <118986817+Akkel-i@users.noreply.github.com>
2023-02-06 17:33:58 +0200

    Update GameEngine.py
    AI movement aboot toimii

--- GameEngine.py
+++ GameEngine.py
@@ -217,21 +217,21 @@ def movement(hahmo):
             the_world[current_position_row][current_position_column] = "--"
       
     if player_movement_input.upper() == "C":
-        if the_world[current_position_row-1][current_position_column+1] == "--":
-            the_world[current_position_row-1][current_position_column+1] = hahmo.id
-            hahmo.position = (current_position_row-1, current_position_column+1)
+        if the_world[current_position_row+1][current_position_column+1] == "--":
+            the_world[current_position_row+1][current_position_column+1] = hahmo.id
+            hahmo.position = (current_position_row+1, current_position_column+1)
             the_world[current_position_row][current_position_column] = "--"
 
     if player_movement_input.upper() == "X":
-        if the_world[current_position_row-1][current_position_column] == "--":
-            the_world[current_position_row-1][current_position_column] = hahmo.id
-            hahmo.position = (current_position_row-1, current_position_column)
+        if the_world[current_position_row+1][current_position_column] == "--":
+            the_world[current_position_row+1][current_position_column] = hahmo.id
+            hahmo.position = (current_position_row+1, current_position_column)
             the_world[current_position_row][current_position_column] = "--"
 
     if player_movement_input.upper() == "Z":
-        if the_world[current_position_row-1][current_position_column-1] == "--":
-            the_world[current_position_row-1][current_position_column-1] = hahmo.id
-            hahmo.position = (current_position_row-1, current_position_column-1)
+        if the_world[current_position_row+1][current_position_column-1] == "--":
+            the_world[current_position_row+1][current_position_column-1] = hahmo.id
+            hahmo.position = (current_position_row+1, current_position_column-1)
             the_world[current_position_row][current_position_column] = "--"
 
     if player_movement_input.upper() == "A":
@@ -240,9 +240,51 @@ def movement(hahmo):
             hahmo.position = (current_position_row, current_position_column-1)
             the_world[current_position_row][current_position_column] = "--"
 
-    else:
-        print("Illegal move, please try again")
+    #else:
+        #print("Illegal move, please try again")
         # loop to beginning
+
+# chekki jos jokin ympäröivä ruutu on vihollinen
+def computer_attack_check(hahmo):
+    has_attacked = False
+    current_position_row = hahmo.position[0]
+    current_position_column = hahmo.position[1]
+    if current_position_row != 0:
+        check_beginning_row = current_position_row - 1
+    else:
+        check_beginning_row = current_position_row
+    if current_position_column != 0:
+        check_beginning_column = current_position_column - 1
+    else:
+        check_beginning_column = current_position_column
+
+    if (check_beginning_row + 2) > 7:
+        limiter_row = 7
+    else:
+        limiter_row = check_beginning_row + 2
+    if (check_beginning_column + 2) > 7:
+        limiter_column = 7
+    else:
+        limiter_column = check_beginning_column + 2       
+    print(check_beginning_row)
+    print(check_beginning_column)
+
+    # tähän saa loopin mikä chekkaa onko mikään ympäröivistä vihollinen . TOIMII muuten paitsi jos ukkeli on 0 tai 7 rivillä nii käy koko loopin läpi
+    while check_beginning_row <= limiter_row and has_attacked == False:
+        while check_beginning_column <= limiter_column:
+            if the_world[check_beginning_row][check_beginning_column] == "GS" or the_world[check_beginning_row][check_beginning_column] == "GB":
+                #if yes,do damage
+                has_attacked = True
+                print("AI hyökkäsi ihmisen kimppuun!")
+                break
+                #pass
+            print(f"{check_beginning_row}, {check_beginning_column}")
+            check_beginning_column += 1   
+        check_beginning_row += 1
+        check_beginning_column = limiter_column - 2 
+    return has_attacked     
+
+
 
 def computer_move(hahmo):
     current_position_row = hahmo.position[0]
@@ -250,68 +292,72 @@ def computer_move(hahmo):
     movement_options_computer = ["Z", "X", "C"]
 
     computer_movement_input = random.choice(movement_options_computer)
-
     print(computer_movement_input)
     # nuolet joka suuntaan näppäimillä: Q W E D C X Z A
     # pitää vielä chekata ettei mene kentän yli tai ali
-    if computer_movement_input == "Q":
-        if the_world[current_position_row-1][current_position_column-1] == "--":
-            the_world[current_position_row-1][current_position_column-1] = hahmo.id
-            hahmo.position = (current_position_row-1, current_position_column-1)
-            the_world[current_position_row][current_position_column] = "--"
-            print("tietokone liikkui : Q")
 
-    if computer_movement_input == "W":
-        if the_world[current_position_row-1][current_position_column] == "--":
-            the_world[current_position_row-1][current_position_column] = hahmo.id
-            hahmo.position = (current_position_row-1, current_position_column)
-            the_world[current_position_row][current_position_column] = "--"
-            print("tietokone liikkui : W")
+    computer_attacked_this_turn = computer_attack_check(hahmo) # tähän hyökkäys chekki ennenkun liikkuu
 
-    if computer_movement_input == "E":
-        if the_world[current_position_row-1][current_position_column+1] == "--":
-            the_world[current_position_row-1][current_position_column+1] = hahmo.id
-            hahmo.position = (current_position_row-1, current_position_column+1)
-            the_world[current_position_row][current_position_column] = "--"
-            print("tietokone liikkui : E")
+    if computer_attacked_this_turn == False:
+        if computer_movement_input == "Q":
+            if the_world[current_position_row-1][current_position_column-1] == "--":
+                the_world[current_position_row-1][current_position_column-1] = hahmo.id
+                hahmo.position = (current_position_row-1, current_position_column-1)
+                the_world[current_position_row][current_position_column] = "--"
+                print("tietokone liikkui : Q")
 
-    if computer_movement_input == "D":
-        if the_world[current_position_row][current_position_column+1] == "--":
-            the_world[current_position_row][current_position_column+1] = hahmo.id
-            hahmo.position = (current_position_row, current_position_column+1)
-            the_world[current_position_row][current_position_column] = "--"
-            print("tietokone liikkui : D")
+        if computer_movement_input == "W":
+            if the_world[current_position_row-1][current_position_column] == "--":
+                the_world[current_position_row-1][current_position_column] = hahmo.id
+                hahmo.position = (current_position_row-1, current_position_column)
+                the_world[current_position_row][current_position_column] = "--"
+                print("tietokone liikkui : W")
+
+        if computer_movement_input == "E":
+            if the_world[current_position_row-1][current_position_column+1] == "--":
+                the_world[current_position_row-1][current_position_column+1] = hahmo.id
+                hahmo.position = (current_position_row-1, current_position_column+1)
+                the_world[current_position_row][current_position_column] = "--"
+                print("tietokone liikkui : E")
+
+        if computer_movement_input == "D":
+            if the_world[current_position_row][current_position_column+1] == "--":
+                the_world[current_position_row][current_position_column+1] = hahmo.id
+                hahmo.position = (current_position_row, current_position_column+1)
+                the_world[current_position_row][current_position_column] = "--"
+                print("tietokone liikkui : D")
       
-    if computer_movement_input == "C":
-        if the_world[current_position_row-1][current_position_column+1] == "--":
-            the_world[current_position_row-1][current_position_column+1] = hahmo.id
-            hahmo.position = (current_position_row-1, current_position_column+1)
-            the_world[current_position_row][current_position_column] = "--"
-            print("tietokone liikkui : C")
+        if computer_movement_input == "C":
+            if the_world[current_position_row+1][current_position_column+1] == "--":
+                the_world[current_position_row+1][current_position_column+1] = hahmo.id
+                hahmo.position = (current_position_row+1, current_position_column+1)
+                the_world[current_position_row][current_position_column] = "--"
+                print("tietokone liikkui : C")
 
-    if computer_movement_input == "X":
-        if the_world[current_position_row-1][current_position_column] == "--":
-            the_world[current_position_row-1][current_position_column] = hahmo.id
-            hahmo.position = (current_position_row-1, current_position_column)
-            the_world[current_position_row][current_position_column] = "--"
-            print("tietokone liikkui : X")
+        if computer_movement_input == "X":
+            if the_world[current_position_row+1][current_position_column] == "--":
+                the_world[current_position_row+1][current_position_column] = hahmo.id
+                hahmo.position = (current_position_row+1, current_position_column)
+                the_world[current_position_row][current_position_column] = "--"
+                print("tietokone liikkui : X")
 
-    if computer_movement_input == "Z":
-        if the_world[current_position_row-1][current_position_column-1] == "--":
-            the_world[current_position_row-1][current_position_column-1] = hahmo.id
-            hahmo.position = (current_position_row-1, current_position_column-1)
-            the_world[current_position_row][current_position_column] = "--"
-            print("tietokone liikkui : Z")
+        if computer_movement_input == "Z":
+            if the_world[current_position_row+1][current_position_column-1] == "--":
+                the_world[current_position_row+1][current_position_column-1] = hahmo.id
+                hahmo.position = (current_position_row+1, current_position_column-1)
+                the_world[current_position_row][current_position_column] = "--"
+                print("tietokone liikkui : Z")
 
-    if computer_movement_input == "A":
-        if the_world[current_position_row][current_position_column-1] == "--":
-            the_world[current_position_row][current_position_column-1] = hahmo.id
-            hahmo.position = (current_position_row, current_position_column-1)
-            the_world[current_position_row][current_position_column] = "--"
-            print("tietokone liikkui : A")
-
-    else:
-        print("Illegal move, please try again")
+        if computer_movement_input == "A":
+            if the_world[current_position_row][current_position_column-1] == "--":
+                the_world[current_position_row][current_position_column-1] = hahmo.id
+                hahmo.position = (current_position_row, current_position_column-1)
+                the_world[current_position_row][current_position_column] = "--"
+                print("tietokone liikkui : A")
+    if computer_attacked_this_turn == True:
+        print("AI ei liikkunut vaan hyökkäsi tällä vuorolla")
+    #else:
+        #print("Illegal move, please try again")
         # loop to beginning
 
 
@@ -400,5 +446,4 @@ if __name__ == '__main__':
     pass
 
 
-# position lisäys pitää lisätä ja bugit siitä pois. Miksei viimeinen  print(swordman_RS.position) toimi,
-#sanoo ettei oo defined, eli ei osaa ottaa sitä vaikka luotu aikasemmin, miksei
+# position lisäys pitää lisätä ja bugit siitä pois.
